d755886066971b41f3c52d94ff27de7ed7a749ec
Rahul Nag <rahulnag44@gmail.com>
2017-12-12 23:55:14 +0530

    Second commit with a different project (.NET framework console app) altogether.

---
+++ SampleConsoleApp/SampleConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Printing something from a config file...");
+            string s = ConfigurationManager.AppSettings["superman"].ToString();
+
+            Console.WriteLine("What I got from the config file is: " + s);
+            Console.WriteLine("Press any key to close...");
+            Console.ReadKey();
+        }
+    }
+}
