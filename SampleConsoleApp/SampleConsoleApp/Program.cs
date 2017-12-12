6cab92894a0e28644862147d59917981434c04e1
Rahul Nag <rahulnag44@gmail.com>
2017-12-12 23:29:15 +0530

    First commit of sample C# Console Application..

---
+++ SampleConsoleApp/SampleConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SampleConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to close...");
+            Console.ReadKey();
+        }
+    }
+}
